88bae27bb283de7d593d93eb399c11fef0e85bfa
Juan <juansfontz@gmail.com>
2021-09-25 02:28:39 -0400

    chore: Add condition to prevent download

--- components/Editor.js
+++ components/Editor.js
@@ -38,48 +38,50 @@ const Editor = (props) => {
         const textToWrite = document.querySelector('#text').value
         const fileNameToSaveAs = 'markdown.md'
 
-        // create a blob elements, which is a file-like object of immutable, raw data; they can be read as text or binary data, or converted into a ReadableStream so its methods can be used for processing the data
-        // read more: https://developer.mozilla.org/en-US/docs/Web/API/Blob
-        var textFileAsBlob = new Blob([textToWrite], {type:'text/plain'})
+        if (textToWrite) {
+          // create a blob elements, which is a file-like object of immutable, raw data; they can be read as text or binary data, or converted into a ReadableStream so its methods can be used for processing the data
+          // read more: https://developer.mozilla.org/en-US/docs/Web/API/Blob
+          var textFileAsBlob = new Blob([textToWrite], {type:'text/plain'})
 
-        // here I going to use the download attribute of the anchor tag to save the text of the textarea triggering the click of the anchor
-        // read more: https://developer.mozilla.org/en-US/docs/Web/HTML/Element/a
+          // here I going to use the download attribute of the anchor tag to save the text of the textarea triggering the click of the anchor
+          // read more: https://developer.mozilla.org/en-US/docs/Web/HTML/Element/a
 
-        var downloadLink = document.createElement("a") // create the anchor tag
-        downloadLink.download = fileNameToSaveAs // assign the name of the file to download including the extension as string (e.g. markdown.md)
-        downloadLink.innerHTML = "Download File" // I don't know the reason of this step, I need a deep research
+          var downloadLink = document.createElement("a") // create the anchor tag
+          downloadLink.download = fileNameToSaveAs // assign the name of the file to download including the extension as string (e.g. markdown.md)
+          downloadLink.innerHTML = "Download File" // I don't know the reason of this step, I need a deep research
 
-        // to download the file I need to use the URL.createObjectURL() API. The URL lifetime is tied to the document in the window on which it was created. The new object URL represents the specified File object or Blob object.
-        // the URL.createObjectURL vary depending of the browser
-        // read more: https://developer.mozilla.org/en-US/docs/Web/API/URL/createObjectURL
-        // more about how to use URL object: https://developer.mozilla.org/en-US/docs/Web/API/File/Using_files_from_web_applications#example_using_object_urls_to_display_images
-        if (window.webkitURL != null) {
-        // Chrome allows the link to be clicked
-        // without actually adding it to the DOM.
-        downloadLink.href = window.webkitURL.createObjectURL(textFileAsBlob)
-        }
-        else {
-          // Firefox requires the link to be added to the DOM
-          // before it can be clicked.
-          downloadLink.href = window.URL.createObjectURL(textFileAsBlob)
-          downloadLink.onclick = () => {
-            destroyClickedElement
-            // revoking the url object to avoid memory leaks
-            window.URL.revokeObjectURL(textFileAsBlob)
-            // console.log('revoking the URL object in mozilla')
+          // to download the file I need to use the URL.createObjectURL() API. The URL lifetime is tied to the document in the window on which it was created. The new object URL represents the specified File object or Blob object.
+          // the URL.createObjectURL vary depending of the browser
+          // read more: https://developer.mozilla.org/en-US/docs/Web/API/URL/createObjectURL
+          // more about how to use URL object: https://developer.mozilla.org/en-US/docs/Web/API/File/Using_files_from_web_applications#example_using_object_urls_to_display_images
+          if (window.webkitURL != null) {
+          // Chrome allows the link to be clicked
+          // without actually adding it to the DOM.
+          downloadLink.href = window.webkitURL.createObjectURL(textFileAsBlob)
           }
-          downloadLink.style.display = "none"
-          document.body.appendChild(downloadLink)
-        }
+          else {
+            // Firefox requires the link to be added to the DOM
+            // before it can be clicked.
+            downloadLink.href = window.URL.createObjectURL(textFileAsBlob)
+            downloadLink.onclick = () => {
+              destroyClickedElement
+              // revoking the url object to avoid memory leaks
+              window.URL.revokeObjectURL(textFileAsBlob)
+              // console.log('revoking the URL object in mozilla')
+            }
+            downloadLink.style.display = "none"
+            document.body.appendChild(downloadLink)
+          }
 
-        downloadLink.click()
-        if (window.webkitURL != null) {
-          // revoking the url object to avoid memory leaks
-          window.webkitURL.revokeObjectURL(textFileAsBlob)
-          // console.log('revoking the URL object in chrome based browser')
-        } else {
-          document.body.removeChild(downloadLink)
-          // console.log('removing child from mozilla')
+          downloadLink.click()
+          if (window.webkitURL != null) {
+            // revoking the url object to avoid memory leaks
+            window.webkitURL.revokeObjectURL(textFileAsBlob)
+            // console.log('revoking the URL object in chrome based browser')
+          } else {
+            document.body.removeChild(downloadLink)
+            // console.log('removing child from mozilla')
+          }
         }
 
         // more about how to upload a file: https://thiscouldbebetter.wordpress.com/2012/12/18/loading-editing-and-saving-a-text-file-in-html5-using-javascrip/
@@ -170,6 +172,7 @@ const Editor = (props) => {
         onChange={e => props.handleChangeM(e.target.value)}
         value={props.markdown}
         onDrop={handleDrop}
+        placeholder='Write your markdown text here'
       >
       </textarea>
     </section>
