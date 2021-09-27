14a5d42c2ae1debdff219aa35e42038c4afaca30
Juan <juansfontz@gmail.com>
2021-09-26 23:30:41 -0400

    chore: Add focus to icons

--- components/Icons.js
+++ components/Icons.js
@@ -5,7 +5,7 @@ const Icons = ({icons}) => {
   return (
     <div>
       {icons.map(i =>
-        <Icon className={styles.icon} icon={i.i} key={i.k} onClick={i.f}/>
+        <Icon className={styles.icon} icon={i.i} key={i.k} onClick={i.f} tabIndex="0"/>
       )}
     </div>
   )
